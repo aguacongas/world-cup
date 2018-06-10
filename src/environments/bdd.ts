349a859f5ebeeefd07cb64c54f2459ac117c05fd
Olivier Lefebvre <olivier.lefebvre@ne.ch>
2018-06-10 09:59:02 +0200

    update bdd.ts

---
+++ src/environments/bdd.ts
@@ -0,0 +1,9 @@
+export const bdd = {
+  firebase: {
+    apiKey: 'Your api key',
+    authDomain: 'Your auth domain',
+    databaseURL: 'Your database url',
+    projectId: 'Your firebase project id'
+  },
+  admin: 'Your admin email address'
+};
